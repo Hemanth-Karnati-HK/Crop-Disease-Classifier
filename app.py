23b131d8c6f8c32fe4483ede52180153d5d4fbd6
Sumanth Karnati <100950254+Sumanthkarnati@users.noreply.github.com>
2023-08-04 16:12:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ from PIL import Image
 import numpy as np
 import json
 
+# Set page title
+st.set_page_config(page_title="Farm Disease Identifier")
+
+# Custom CSS for styling
+st.markdown("""
+<style>
+    .reportview-container {
+        background: #f0f0f5;
+    }
+    h1 {
+        color: #2e7d32;
+    }
+    .stButton>button {
+        background-color: #2e7d32;
+        color: white;
+    }
+</style>
+""", unsafe_allow_html=True)
+
 # Load the trained model
 model_path = 'full_model.h5'
 model = tf.keras.models.load_model(model_path)
@@ -19,8 +38,8 @@ with open('cure.json', 'r') as f:
 def predict(image):
     # Preprocess the image to match the model's input shape
     image_resized = image.resize((128, 128))
-    image_array = np.array(image_resized) / 255.0
-    image_array = image_array[np.newaxis, ...]
+    image_array = np.array(image_resized) / 255.0  # Normalize
+    image_array = image_array[np.newaxis, ...]  # Add batch dimension
 
     # Make a prediction
     prediction = model.predict(image_array)
@@ -29,51 +48,56 @@ def predict(image):
     # Return both class index and disease name
     return predicted_class, index_to_class[str(predicted_class)]
 
-def translate_text(description):
-    st.write("Translate Description:")
-    # User input for the text to translate
+def translate_text(text):
+    # User input for the target language
     target_language = st.selectbox("Select the target language:", ('', 'Telugu', 'Tamil', 'Hindi'))
 
     # Language codes corresponding to the selected languages
     language_codes = {'Telugu': 'te', 'Tamil': 'ta', 'Hindi': 'hi'}
 
-    # Create a URL to translate the text using Google Translate
     if target_language:
         language_code = language_codes[target_language]
-        description_truncated = description[:100]
-        translate_url = f"https://translate.google.com/?sl=auto&tl={language_code}&text={description_truncated}&op=translate"
+        translate_url = f"https://translate.google.com/?sl=auto&tl={language_code}&text={text}&op=translate"
         st.markdown(f"[Click here to translate the text into {target_language} using Google Translate]({translate_url})")
 
 def display_prediction(predicted_disease_name, disease_descriptions):
     description = disease_descriptions.get(predicted_disease_name, "Description not available")
-    st.write("Prediction:", predicted_disease_name)
-    st.subheader("Description")
-    st.write(description.split("Prevention")[0])
-
-    if "Prevention" in description:
-        st.subheader("Prevention")
-        prevention_description = description.split("Prevention")[1]
-        st.write(prevention_description)
-
-    st.write("If you have any concerns or need further assistance, please consult with a local agricultural expert.")
+    st.success(f"Prediction: **{predicted_disease_name}**")
+    st.subheader("Description 📖")
     
-    # Add a button to trigger the translation section
+    # Splitting the description into main content and prevention
+    description_parts = description.split("Prevention")
+    
+    st.write(description_parts[0])
+    
+    if len(description_parts) > 1:
+        st.subheader("Prevention 💡")
+        st.write(description_parts[1])
+    else:
+        st.warning("No specific prevention details available for this disease.")
+
+    st.write("If you have any concerns or need further assistance, please consult with a local agricultural expert. 🚜")
+    
     if st.button('Translate Description'):
         translate_text(description)
 
-st.title("Farm Disease Identifier")
-st.write("Welcome! Upload an image of a plant, and I'll identify the disease (if any). Together, we can ensure a healthy crop!")
+st.title("🌱 Farm Disease Identifier 🌾")
+st.write("Welcome to the Farm Disease Identifier! Upload an image of a plant, and I'll identify the disease (if any). Together, we can ensure a healthy crop! 🌻")
 
 # Upload image
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+uploaded_file = st.file_uploader("Choose an image (jpg or png)...", type=["jpg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Classifying... 🧠")
 
     # Predict the class of the uploaded image
     predicted_class, predicted_disease_name = predict(image)
 
-    # Display the prediction
+    # Display human-readable label and detailed description
     display_prediction(predicted_disease_name, disease_descriptions)
+
+# Footer
+st.markdown("---")
+st.markdown("Developed with ❤️ for farmers everywhere!")
